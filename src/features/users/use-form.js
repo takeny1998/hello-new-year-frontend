10c375238cded4d373a2727fdcd1a0fdfb8445fe
Heebeom Yang <takeny1998@gmail.com>
2023-05-28 20:59:28 +0900

    feat(use-form.js): add form custom hook
    - 회원가입, 로그인을 대체한 custom hook 생성

---
+++ src/features/users/use-form.js
@@ -0,0 +1,38 @@
+import { useCallback, useState } from "react";
+
+
+const useForm = (initValues) => {
+    const [ values, setValues ] = useState(initValues);
+
+    const changeHandler = (event) => {
+        const { name, value } = event.target;
+        
+        setValues((prevState) => {
+            return { ...prevState, [name]: value };
+        });
+    };
+
+    const submitHandler = (event, onSubmit, validate) => {
+        event.preventDefault();
+        const errors = validate(values);
+
+        let errorMessage = '';
+
+        for (const error in errors) {
+            errorMessage += `[${error}] ${errors[error]}\n`;
+        }
+        
+        if (errorMessage.length === 0) {
+            onSubmit(values);
+        } else {
+            alert(errorMessage);
+        }
+    };
+
+    return {
+        changeHandler,
+        submitHandler
+    };
+}
+
+export default useForm;
