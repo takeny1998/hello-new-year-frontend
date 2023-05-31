de02a9a5d41bf590d6f10f27de11553bba6ae920
Heebeom Yang <takeny1998@gmail.com>
2023-05-31 13:20:14 +0900

    reactor(use-http.js): error handling 개선
    - 백엔드 개발자와 회의 후, 현재 반환하는 status code를 종합하고, 위험 정도에 따라 분리
    - frontend에서 위험도에 따라 error respoinse를 분리하고, 각각에 맞는 handling logic을 적용

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -27,24 +27,43 @@ const useHttp = () => {
       onComplete(resData.result);
     } catch (err) {
       /*
-                TODO: 404 not found (객체 리소스 없음, URL 없음) 에 대해서
-                Backend 개발자와 대화 필요
+          TODO: 404 not found (객체 리소스 없음, URL 없음) 에 대해서
+          Backend 개발자와 대화 필요
 
-                현재는 객체 리소스가 없는 경우, 해당 Uri 요청이 없는 경우 모두
-                404 not found를 반환하고 있음
-            */
+          현재는 객체 리소스가 없는 경우, 해당 Uri 요청이 없는 경우 모두
+          404 not found를 반환하고 있음
+      */
+
+      const errData = await err.res.json();
       switch (err.res.status) {
+        /*
+          [중대한 위험]: 401 unAuthentication
+          사용자 인증 정보나, 보호된 자원에 잘못 접근한 경우
+          사용자의 인증 정보를 Clear 한 다음,
+          메인 페이지로 redirect 해야 함 
+        */
         case 401:
+          alert(errData.result.message);
+          dispatch(logout());
+          navigate("/");
+          break;
+        /*
+          [경미한 위험]: 404 not Found, 409 Conflict
+          중복된 자원을 생성하거나, 없는 자원을 참조하는 경우
+          페이지를 refresh 해야 함
+         */
         case 404:
         case 409:
-          const errData = await err.res.json();
           alert(errData.result.message);
+          window.location.reload();
           break;
+        /*
+          [기타 위험]
+         */
         default:
           alert("서버와의 통신에 문제가 발생했습니다.");
+          window.location.reload();
       }
-      dispatch(logout());
-      navigate("/");
     }
 
     setIsLoading(false);
